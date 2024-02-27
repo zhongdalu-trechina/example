5c127ade93580f5d5e17e69cf8545aa2f6cc89af
钟大陆 <10188142zhong_dalu@cn.tre-inc.com>
2024-02-27 13:55:39 +0800

    aaa

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/getsentry/sentry-go"
+	sentryecho "github.com/getsentry/sentry-go/echo"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	_ = sentry.Init(sentry.ClientOptions{
+		Dsn:         "https://6299f691eb724b1c936ab59d6fe287c8@o410846.ingest.sentry.io/6151500",
+		ServerName:  "test",
+		Environment: "local",
+		BeforeSend: func(event *sentry.Event, hint *sentry.EventHint) *sentry.Event {
+			if hint.Context != nil {
+				if req, ok := hint.Context.Value(sentry.RequestContextKey).(*http.Request); ok {
+					// You have access to the original Request
+					fmt.Println(req)
+				}
+			}
+			fmt.Println(event)
+			return event
+		},
+		Debug:            true,
+		AttachStacktrace: true,
+	})
+
+	app := echo.New()
+
+	app.Use(middleware.Logger())
+	app.Use(middleware.Recover())
+
+	app.Use(sentryecho.New(sentryecho.Options{
+		Repanic: true,
+	}))
+
+	app.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			if hub := sentryecho.GetHubFromContext(ctx); hub != nil {
+				hub.Scope().SetTag("someRandomTag", "maybeYouNeedIt")
+			}
+			return next(ctx)
+		}
+	})
+
+	app.GET("/", func(ctx echo.Context) error {
+		if hub := sentryecho.GetHubFromContext(ctx); hub != nil {
+			hub.WithScope(func(scope *sentry.Scope) {
+				scope.SetExtra("unwantedQuery", "someQueryDataMaybe")
+				hub.CaptureMessage("User provided unwanted query string, but we recovered just fine")
+			})
+		}
+		return ctx.String(http.StatusOK, "Hello, World!")
+	})
+
+	app.GET("/foo", func(ctx echo.Context) error {
+		// sentryecho handler will catch it just fine, and because we attached "someRandomTag"
+		// in the middleware before, it will be sent through as well
+		panic("y tho")
+	})
+
+	app.Logger.Fatal(app.Start(":3000"))
+}
